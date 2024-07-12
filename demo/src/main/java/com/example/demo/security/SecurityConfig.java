287b27eeed59886e760af9d696306a538295f81c
yangyongxiang02 <yangyongxiang02@meituan.com>
2024-07-12 15:17:29 +0800

    5.3结束（未适配注册界面验证

--- demo/src/main/java/com/example/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,9 +1,22 @@
 package com.example.demo.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
 
 @Configuration
 public class SecurityConfig {
@@ -12,5 +25,37 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+  List<UserDetails> usersList = new ArrayList<>();
+  usersList.add(new User("buzz", encoder.encode("password"),Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+  usersList.add(new User("woody", encoder.encode("password"),Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+  return new InMemoryUserDetailsManager(usersList);
+  }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests()
+        .requestMatchers("/design", "/orders").hasRole("USER")
+        .anyRequest().permitAll()
+  
+      .and()
+        .formLogin()
+          .loginPage("/login")
+  
+      .and()
+        .oauth2Login()
+          .loginPage("/login")
+  
+
+      .and()
+        .logout()
+          .logoutSuccessUrl("/")
+      .and()
+        .build();
+  }
+
 
 }
